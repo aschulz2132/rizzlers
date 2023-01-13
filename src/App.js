7de05c5650098859c16789f496891c723fa33c04
aschulz2132 <aschulz2132@gmail.com>
2023-01-13 02:38:02 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import "./styles.css";
+import { useState, onMount } from "react";
+import { useEffect } from "react/cjs/react.production.min";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Rizzlers.org</h1>
-      <h5>play</h5>
-    </div>
-  );
+  const [bruh, setBruh] = useState(false);
+  const [count, setCount] = useState(0);
+  function pee() {
+    navigator.clipboard.writeText("mc.rizzlers.org");
+  }
+  if (bruh) {
+    return (
+      <div className="purpleBack">
+        <div className="green">
+          <h1>Rizzlers.org</h1>
+
+          <h5>play</h5>
+          <p>click to copy to clipboard</p>
+          <div className="E" onClick={pee}>
+            <h3>mc.rizzlers.org</h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  if (!bruh) {
+    return (
+      <div className="greenBack">
+        <div className="purple">
+          <h1>Rizzlers.org</h1>
+
+          <h5>play</h5>
+          <p>click to copy to clipboard</p>
+          <div className="E" onClick={pee}>
+            <h3>mc.rizzlers.org</h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
